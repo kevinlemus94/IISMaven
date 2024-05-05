d2e4b5f2af3ad57d037c325adf45920ec164ebb0
Kevin Lemus <nkevinmanuel94@gmail.com>
2024-05-05 11:43:23 -0600

    Guia Practica 4

---
+++ src/test/java/com/iis/LN13002/demo_LN13002/AppTest.java
@@ -0,0 +1,16 @@
+package com.iis.LN13002.demo_LN13002;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
